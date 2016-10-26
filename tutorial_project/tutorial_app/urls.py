df782ef7931c6f6787b6bf1bcd5010e7278dd7ee
Nohely <nohelyvperez@gmail.com>
2016-10-25 20:54:44 -0400

    Hyperlinks

--- tutorial_project/tutorial_app/urls.py
+++ tutorial_project/tutorial_app/urls.py
@@ -3,4 +3,6 @@ from tutorial_app import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'^about/', views.about, name='about'))
+    url(r'^about/', views.about, name='about'),
+	url(r'^categories/(Category_name_slug>[w-]+)/$', views.category, name='category'),
+	)
