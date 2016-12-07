209b7d9f1edf5321010de3b5f375ab49740d4d41
Takwaun Best <takwaunbest@Takwauns-MacBook-Air.local>
2016-12-06 20:49:16 -0500

    AutoAddPage-FIX BING

--- tutorial_project/static/js/app_ajax.js
+++ tutorial_project/static/js/app_ajax.js
@@ -1,4 +1,4 @@
-$(document).ready(function){
+$(document).ready(function(){
 
 	//like button
 
@@ -11,6 +11,29 @@ $(document).ready(function){
 			$('#likes').hide();
 		});
 	})
+
+	$('#suggestion').keyup(function(){
+		var query;
+		query= $(this).val();
+		$.get('/suggest_category/',{suggestion:query}, function(data){
+			$('#cats').html(data);
+		});
+	});
+
+
+	$('.tutorial-add').click(function(){
+		var cat_id= $(this).attr('data-catid');
+		var url = $(this).attr("data-url");
+		var title = $(this).attr("data-title");
+		var user = $(this).attr("data-user");
+		var men $(this);
+
+		$.get('/auto_add_page/', {category_id : cat_id, url: url, title: title, user:user}, 
+			function(data){
+				$('#pages').html(data);
+				me.hide();
+			});
+	});
 
 
 
